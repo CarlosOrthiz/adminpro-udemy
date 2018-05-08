62307450b0cc1264023aef0f582d9c7010499caa
Carlos Orthiz <carlosorthiz@hotmail.com>
2018-05-08 14:09:38 -0600

    Input, output y viewchild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { APP_ROUTES } from './app.routes';
 
 // Modulos
 import { PagesModule } from './pages/pages.module';
+import { FormsModule } from '@angular/forms';
 
 // Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
@@ -18,8 +20,15 @@ import { RegisterComponent } from './login/register.component';
     LoginComponent,
     RegisterComponent
   ],
-  imports: [BrowserModule, APP_ROUTES, PagesModule],
+  imports: [
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule,
+    FormsModule
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,16 +3,26 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
 // Modulos
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 import { PAGES_ROUTES } from './pages.routes';
+
+import { ChartsModule } from 'ng2-charts';
+
 
 @NgModule({
   declarations: [
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent,
+    GraficoDonaComponent
   ],
   exports: [
     PagesComponent,
@@ -22,7 +32,9 @@ import { PAGES_ROUTES } from './pages.routes';
   ],
   imports: [
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule,
+    ChartsModule
   ]
 })
 export class PagesModule {}
